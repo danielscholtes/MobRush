1437894dc09035ee927e2abd578e3d4e4ac0d008
danielscholtes <familiescholteshoofddorp@gmail.com>
2021-06-30 15:22:45 +0300

    Added MobRushGame base, will continue once other PRs have been merged

---
+++ src/main/java/me/scholtes/mobrush/game/Game.java
@@ -0,0 +1,15 @@
+package me.scholtes.mobrush.game;
+
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public interface Game {
+
+    void startGame();
+    void endGame();
+    void setStatus(GameStatus status);
+    GameStatus getStatus();
+    List<Player> getPlayers();
+
+}
---
+++ src/main/java/me/scholtes/mobrush/game/mobrushgame/MobRushGame.java
@@ -0,0 +1,116 @@
+package me.scholtes.mobrush.game.mobrushgame;
+
+import me.scholtes.mobrush.MobRushPlugin;
+import me.scholtes.mobrush.game.Game;
+import me.scholtes.mobrush.game.GameStatus;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MobRushGame implements Game, Listener {
+
+    private List<Player> players;
+    private GameStatus status;
+    private int currentWave;
+    private int START_INTERVAL = 60;
+    private int INTERVAL_DECREASE = 5;
+    private int MIN_INTERVAL = 5;
+    private int currentWaveTask = -1;
+    //TODO: Add a Map<UUID, Kit>
+
+    /**
+     * Whenever a lobby is created an instance of a game will be made
+     */
+    public MobRushGame() {
+        this.players = new ArrayList<>();
+        this.status = GameStatus.WAITING;
+        setCurrentWave(0);
+    }
+
+    /**
+     * Starts the game and lets players choose kits, after 10 seconds the first wave starts
+     */
+    @Override
+    public void startGame() {
+        setStatus(GameStatus.RUNNING);
+
+        //TODO: Teleport players to world
+        //TODO: Add kit selection (10 seconds to choose kit, otherwise defaults to knight kit)
+    }
+
+    /**
+     * Progresses to the next wave and adds a runnable that will
+     * decrease by 5 seconds after every wave. The runnable will begin then next wave
+     */
+    private void nextWave() {
+        //TODO: Mob spawning logic
+
+        // Determines the delay till the next wave
+        int delay = START_INTERVAL - (getCurrentWave() * INTERVAL_DECREASE);
+        delay = Math.max(delay, MIN_INTERVAL);
+        this.currentWaveTask = new BukkitRunnable() {
+            @Override
+            public void run() {
+                nextWave();
+            }
+        }.runTaskLaterAsynchronously(MobRushPlugin.getInstance(), delay * 20L).getTaskId();
+        setCurrentWave(getCurrentWave() + 1);
+    }
+
+    /**
+     * Ends the game
+     */
+    @Override
+    public void endGame() {
+        //TODO: Send the remaining player(s) back to the lobby
+
+        if (currentWaveTask != -1) {
+            Bukkit.getScheduler().cancelTask(currentWaveTask);
+        }
+        players.clear();
+        setCurrentWave(0);
+        setStatus(GameStatus.WAITING);
+    }
+
+    /**
+     * Sets the current wave
+     */
+    private void setCurrentWave(int wave) {
+        this.currentWave = wave;
+    }
+
+    /**
+     * Gets the current wave
+     */
+    private int getCurrentWave() {
+        return this.currentWave;
+    }
+
+    /**
+     * Sets the status of the game
+     */
+    @Override
+    public void setStatus(GameStatus status) {
+        this.status = status;
+    }
+
+    /**
+     * Gets the status of the game
+     */
+    @Override
+    public GameStatus getStatus() {
+        return status;
+    }
+
+    /**
+     * Gets the players currently in the game
+     */
+    @Override
+    public List<Player> getPlayers() {
+        return players;
+    }
+}
