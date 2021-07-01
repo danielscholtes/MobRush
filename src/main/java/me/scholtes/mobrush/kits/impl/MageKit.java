cce0fd703f055f5216a8115544660a2735f08ac2
danielscholtes <familiescholteshoofddorp@gmail.com>
2021-07-01 19:08:29 +0300

    Added Mage Kit

---
+++ src/main/java/me/scholtes/mobrush/kits/impl/MageKit.java
@@ -0,0 +1,14 @@
+package me.scholtes.mobrush.kits.impl;
+
+import me.scholtes.mobrush.itembuilder.ItemBuilder;
+import me.scholtes.mobrush.kits.RoundKit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+public class MageKit implements RoundKit {
+
+    @Override
+    public void nextRound(Player player) {
+        player.getInventory().addItem(new ItemBuilder(Material.ENDER_PEARL).build());
+    }
+}
